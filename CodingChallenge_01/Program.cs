627f1de80c7ca6d788c516a3bf1d59495c43c8d2
AlexBhTec <22340629@itnogales.edu.mx>
2025-11-06 10:08:19 -0700

    Added Both Solutions

---
+++ CodingChallenge_01/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using HelloWorld;
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Circle
+            Circle objcircle = new Circle();
+            Console.WriteLine(objcircle.getArea());
+
+            //Rectangle
+            Rectangle objRectangle = new Rectangle();
+            Console.WriteLine(objRectangle.getArea());
+
+            //Triangle
+
+            Triangle objTriangle = new Triangle();
+            Console.WriteLine(objTriangle.getArea());
+
+        }
+
+
+    }
+
+
+    public class Shape()
+    {
+        public virtual double getArea()
+        {
+            double formula = 0;
+
+            return (formula);
+        }
+
+
+    }
+
+
+    public class Circle : Shape
+
+    {
+        private double radius = 5;
+
+        public override double getArea()
+        {
+            double formula;
+            formula = 3.1416 * radius * radius;
+            return (formula);
+        }
+    }
+
+
+    public class Rectangle : Shape
+
+    {
+        private double width = 6;
+        private double height = 4;
+
+        public override double getArea()
+        {
+            double formula;
+            formula = width * height;
+            return (formula);
+        }
+    }
+
+
+    public class Triangle : Shape
+
+    {
+        private double _base = 3;
+        private double height = 4;
+
+        public override double getArea()
+        {
+            double formula;
+            formula = (_base * height) / 2;
+            return (formula);
+        }
+    }
+
+}
---
+++ CodingChallenge_02/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+    public static class Program
+    {
+        public static int a = 0;
+        public static int b = 1;
+        public static int c;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(b); 
+            FibonacciOf(9);
+        }
+
+
+        public static void FibonacciOf(int n)
+        {
+            
+            for (int i = 1; i <= n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(c);
+            }
+        }
+    }
+
+
+    
+}
+
+
+
+/*PseudoCode
+ * int a = 0;
+int b = 1;
+
+FibonacciOf(int n) {
+
+    for (int i = 1; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+//c is being declared but is never being printed
+    }
+}
+
+// Main Function
+main() {
+    print(a);
+    print(b);
+    FibonacciOf(10);
+//As the challengue requires to print eleven numbers of the sequence, and we have already printed 2, we must only print 9 numbers from the function 
+}
+ * 
+ * 
+    */
